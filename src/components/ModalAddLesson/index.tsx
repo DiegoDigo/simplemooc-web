331d17f4e154f13f706b1fadaa44e75847918c86
Diego Domingos Delmiro <diego.delmiro@totvs.com.br>
2021-06-01 08:58:09 -0300

    fix: ajustando imports e criando reset form

--- src/components/ModalAddLesson/index.tsx
+++ src/components/ModalAddLesson/index.tsx
@@ -1,22 +1,21 @@
-import React, {useEffect, useState} from 'react';
-import {Button, ButtonClose, Container, Description, InfoWrapper, Title, WrapperButton} from './styles';
-import {useHistory} from "react-router-dom";
+import { Formik } from "formik";
+import React, { useEffect, useState } from 'react';
 import ModalAddLessonModel from "../../core/models/ModalAddLessonModel";
-import {FormStyle} from "../../pages/LoginPage/styles";
+import { LessonSchemaValidator } from "../../core/models/Validators/course.validator";
+import { LessonRequest } from "../../data/models/Request/LessonRequest";
+import { createLesson } from "../../data/services/lessonService";
+import { FormStyle } from "../../pages/LoginPage/styles";
 import Input from "../Input";
-import {Formik} from "formik";
 import InputFile from "../InputFile";
-import {LessonSchemaValidator} from "../../core/models/Validators/course.validator";
-import {LessonRequest} from "../../data/models/Request/LessonRequest";
-import {createLesson} from "../../data/services/lessonService";
-import Modal from "../Modal";
 import Loading from "../Loading";
+import Modal from "../Modal";
+import { Button, ButtonClose, Container, Description, InfoWrapper, Title, WrapperButton } from './styles';
 
 
 const ModalAddLesson: React.FC<ModalAddLessonModel> = ({course, show, setShowParent}) => {
 
     const initialValues: LessonRequest = {courseId: course.id, name: "", material: undefined, description: ""};
-    const history = useHistory();
+    
     const [isShow, setIsShow] = useState(false);
     const [loading, setLoading] = useState(false);
     const [isShowInfo, setIsShowInfo] = useState(false);
@@ -27,16 +26,16 @@ const ModalAddLesson: React.FC<ModalAddLessonModel> = ({course, show, setShowPar
     }, [show, course])
 
 
-    const addLesson = async (values: LessonRequest) => {
+    const addLesson = async (values: LessonRequest, reset: Function) => {
         values.courseId = course.id;
         setLoading(true);
         await createLesson(values)
             .then(resp => {
                 if (resp.status === 201 && resp.data.success) {
-                    history.go(0);
                     setIsShowInfo(false);
                     setError(false);
                     setLoading(false);
+                    reset();
                 }
             }).catch(_ => {
                 setIsShowInfo(true);
@@ -57,8 +56,8 @@ const ModalAddLesson: React.FC<ModalAddLessonModel> = ({course, show, setShowPar
                 <Title>{course.name}</Title>
                 <Description>Adicionar Aulas</Description>
                 <Formik initialValues={initialValues} validationSchema={LessonSchemaValidator}
-                        onSubmit={(values) => {
-                            addLesson(values);
+                        onSubmit={(values, {resetForm}) => {
+                            addLesson(values, resetForm);
                         }}>
                     {(formik) => {
                         const {errors, setFieldValue} = formik;
