081c24ba147dfb47e7c111f873051204a9a4b449
Diego Domingos Delmiro <diego.delmiro@totvs.com.br>
2021-06-10 11:34:32 -0300

    fix: ajuste na tela de home page

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -10,7 +10,7 @@ const Navbar: React.FC = () => {
     const history = useHistory();
 
     const logout = () => {
-        localStorage.clear();
+        sessionStorage.clear();
         setAuthenticated(false);
         setRole("");
         history.push("/")
--- src/core/util/localstorage.util.ts
+++ src/core/util/localstorage.util.ts
@@ -1,7 +1,7 @@
 export const setLocalStorage = (key: string, value: string | any): void => {
-    localStorage.setItem(key, value);
+    sessionStorage.setItem(key, value);
 }
 
 export const getLocalStorage = (key: string): any => {
-    return localStorage.getItem(key);
+    return sessionStorage.getItem(key);
 }
